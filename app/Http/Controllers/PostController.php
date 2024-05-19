4a5b0baec18f975482dc419afc977e4e19216458
marwoto123 <sul.hmzz@gmail.com>
2024-05-19 19:31:16 +0000

    tambah pencarian di body

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
         // dd(request('search'));
         $posts = Post::latest();
         if (request('search')) {
-            $posts->where('title', 'like', '%' . request('search') . '%');
+            $posts->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%' );
         }
 
 
